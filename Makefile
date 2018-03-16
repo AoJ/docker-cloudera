2b7b4b120d47a4c962bb55f20886cf1310644057
AoJ <aoj@n13.cz>
2018-03-16 10:58:10 +0100

    some another chages
    Signed-off-by: AoJ <aoj@n13.cz>

--- Makefile
+++ Makefile
@@ -1,18 +1,18 @@
 NAME ?= aooj/cdh
-VERSION ?= 0.10.3
+VERSION ?= 5.13.2
 CDH_VERSION ?= 5.13.2
 
 root:
 	docker build --no-cache --force-rm -t ${NAME}:root root
 
-cdh-base:
-	docker build --no-cache --force-rm --build-arg CDH_VERSION=${VERSION} -t ${NAME}-base:${VERSION} .
+base:
+	docker build --no-cache --force-rm --build-arg CDH_VERSION=${VERSION} -t ${NAME}-base:${VERSION} cdh-base
 
-cdh-base2:
-	docker build --no-cache --force-rm --build-arg CDH_VERSION=${VERSION} -t ${NAME}-base2:${VERSION} .
+base2:
+	docker build --no-cache --force-rm --build-arg CDH_VERSION=${VERSION} -t ${NAME}-base2:${VERSION} cdh-base2
 
-cdh-node:
-	docker build --no-cache --force-rm --build-arg CDH_VERSION=${VERSION} -t ${NAME}-node:${VERSION} .
+node:
+	docker build --no-cache --force-rm --build-arg CDH_VERSION=${VERSION} -t ${NAME}-node:${VERSION} chd-node
 
 
 .PHONY: root cdh-base cdh-base2 cdh-node
--- cdh-base/files/cdh_download.sh
+++ cdh-base/files/cdh_download.sh
@@ -1,28 +1,53 @@
 #!/usr/bin/env bash
 set -exuo pipefail
 
-cat << CMD > /etc/yum.repos.d/cm.repo
-[cloudera-manager]
-name=Cloudera Manager
-baseurl=https://archive.cloudera.com/cm5/redhat/7/x86_64/cm/${CDH_VERSION}/
-gpgkey =https://archive.cloudera.com/cm5/redhat/7/x86_64/cm/RPM-GPG-KEY-cloudera
-gpgcheck = 1
-CMD
+ansible-playbook -vv /dev/stdin <<ANS
+---
+- hosts: localhost
+  tasks:
+  -  yum_repository:
+        name: cloudera-manager
+        description: Cloudera Manager repository
+        baseurl: https://archive.cloudera.com/cm5/redhat/7/x86_64/cm/{{ lookup('env','CDH_VERSION') }}/
+        gpgkey: https://archive.cloudera.com/cdh5/redhat/7/x86_64/cdh/RPM-GPG-KEY-cloudera
+        gpgcheck: yes
+        enabled: yes
+  - yum:
+        name: cloudera-manager-daemons
+  - yum:
+        name: cloudera-manager-agent
+  - yum:
+        name: cloudera-manager-server
+  - yum:
+        name: oracle-j2sdk1.7
 
-rpm --import https://archive.cloudera.com/cdh5/redhat/7/x86_64/cdh/RPM-GPG-KEY-cloudera
+  - systemd:
+        name: cloudera-scm-server
+        enabled: no
+  - systemd:
+        name: cloudera-scm-agent
+        enabled: no
 
-yum install -y wget cloudera-manager-daemons cloudera-manager-agent cloudera-manager-server oracle-j2sdk1.7
+  - file:
+      path: /opt/cloudera/parcel-repo
+      state: directory
+      mode: 0755
+      owner: cloudera-scm
+      group: cloudera-scm
+  
+  - get_url:
+      url: "https://archive.cloudera.com/cdh5/parcels/{{ lookup('env','CDH_VERSION') }}/CDH-{{ lookup('env','CDH_VERSION') }}-1.cdh{{ lookup('env','CDH_VERSION') }}.p0.3-el7.parcel"
+      dest: /opt/cloudera/parcel-repo/
+
+  - get_url:
+      url: "http://archive.cloudera.com/cdh5/parcels/{{ lookup('env','CDH_VERSION') }}/manifest.json"
+      dest: /opt/cloudera/parcel-repo/
+
+  - get_url:
+      url: "https://archive.cloudera.com/cdh5/parcels/{{ lookup('env','CDH_VERSION') }}/CDH-{{ lookup('env','CDH_VERSION') }}-1.cdh{{ lookup('env','CDH_VERSION') }}.p0.3-el7.parcel.sha1"
+      dest: "/opt/cloudera/parcel-repo/CDH-{{ lookup('env','CDH_VERSION') }}-1.cdh{{ lookup('env','CDH_VERSION') }}.p0.3-el7.parcel.sha"
+
+ANS
+
 yum clean all
 rm -rf /var/cache/yum
-
-systemctl disable cloudera-scm-server
-systemctl disable cloudera-scm-agent
-
-ansible-playbook -vv ${BASH_SOURCE%/*}/cdh_download.yml
-
-# mkdir -p /opt/cloudera/parcel-repo
-# cd /opt/cloudera/parcel-repo
-# wget -q https://archive.cloudera.com/cdh5/parcels/${CDH_VERSION}/CDH-${CDH_VERSION}-1.cdh${CDH_VERSION}.p0.3-el7.parcel
-# wget -q https://archive.cloudera.com/cdh5/parcels/${CDH_VERSION}/CDH-${CDH_VERSION}-1.cdh${CDH_VERSION}.p0.3-el7.parcel.sha1
-# cp CDH-${CDH_VERSION}-1.cdh${CDH_VERSION}.p0.3-el7.parcel.sha1 CDH-${CDH_VERSION}-1.cdh${CDH_VERSION}.p0.3-el7.parcel.sha
-# wget -q http://archive.cloudera.com/cdh5/parcels/${CDH_VERSION}/manifest.json
